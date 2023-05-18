8ea781ee62c226eb04cc2201a8792c1a70c63fe3
Nazar <youtube@superneon4ik.me>
2023-05-18 17:35:38 +0300

    Remove this silly "Running Noxesium v1.4" message if up-to-date

--- src/main/java/me/superneon4ik/noxesiumutils/modules/UpdateChecker.java
+++ src/main/java/me/superneon4ik/noxesiumutils/modules/UpdateChecker.java
@@ -49,10 +49,10 @@ public class UpdateChecker {
             String latestReleaseTag = jsonObj.getString("tag_name");
             if (!latestReleaseTag.equalsIgnoreCase(plugin.getDescription().getVersion())) {
                 sender.sendMessage(ChatColor.YELLOW + "Your NoxesiumUtils may be outdated! Latest release version is " + latestReleaseTag + ". Download: " + ChatColor.WHITE + jsonObj.getString("html_url"));
+                sender.sendMessage(ChatColor.AQUA + "Running " + plugin.getName() + " v" + plugin.getDescription().getVersion() + ".");
             }
         } catch (IOException ignore) {
             sender.sendMessage(ChatColor.RED + "Failed to check for updates.");
         }
-        sender.sendMessage(ChatColor.AQUA + "Running " + plugin.getName() + " v" + plugin.getDescription().getVersion() + ".");
     }
 }
