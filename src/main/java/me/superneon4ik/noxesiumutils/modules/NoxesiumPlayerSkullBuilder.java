ac04e12b9765adcc9c8e57694723b4df50f896a2
Nazar <youtube@superneon4ik.me>
2023-05-18 17:33:49 +0300

    NoxesiumPlayerSkullBuilder. Bump plugin version

---
+++ src/main/java/me/superneon4ik/noxesiumutils/modules/NoxesiumPlayerSkullBuilder.java
@@ -0,0 +1,63 @@
+package me.superneon4ik.noxesiumutils.modules;
+
+import me.superneon4ik.noxesiumutils.NoxesiumUtils;
+import net.kyori.adventure.text.Component;
+import net.md_5.bungee.api.chat.ScoreComponent;
+
+import java.util.UUID;
+
+public class NoxesiumPlayerSkullBuilder {
+    private UUID uuid;
+    private boolean grayscale = false;
+    private int advance = 0;
+    private int ascent = 0;
+    private float scale = 1.0f;
+
+    public NoxesiumPlayerSkullBuilder(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public NoxesiumPlayerSkullBuilder setUniqueID(UUID value) {
+        uuid = value;
+        return this;
+    }
+
+    public NoxesiumPlayerSkullBuilder setGrayscale(boolean value) {
+        grayscale = value;
+        return this;
+    }
+
+    public NoxesiumPlayerSkullBuilder setAdvance(int value) {
+        advance = value;
+        return this;
+    }
+
+    public NoxesiumPlayerSkullBuilder setAscent(int value) {
+        ascent = value;
+        return this;
+    }
+
+    public NoxesiumPlayerSkullBuilder setScale(float value) {
+        scale = value;
+        return this;
+    }
+
+    public String buildString() {
+        var args = new String[] {
+                uuid.toString(),
+                String.valueOf(grayscale),
+                String.valueOf(advance),
+                String.valueOf(ascent),
+                String.valueOf(scale)
+        };
+        return "%NCPH%" + String.join(",", args);
+    }
+
+    public ScoreComponent buildSpigot() {
+        return new ScoreComponent(buildString(), "");
+    }
+
+    public Component build() {
+        return Component.score(buildString(), "");
+    }
+}
